1c355d5f252121901e859b1a33e51d1ae07a6849
Vl1704 <vl.x@bk.ru>
2025-01-10 16:34:47 +0300

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,1 +1,22 @@
-ip a
+apt-get install wget
+install -d -m 0755 /etc/apt/keyrings
+
+#Import the Mozilla APT repository signing key:
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+
+#The fingerprint should be **35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3**. You may check it with the following command:
+gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
+
+#Adding the Mozilla APT repository to your sources list
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo '
+    Package: *
+    Pin: origin packages.mozilla.org
+    Pin-Priority: 1000
+    ' | tee /etc/apt/preferences.d/mozilla
+apt-get update
+apt-get install firefox
+
+#set Firefox as default Web Browser
+update-alternatives --install /usr/bin/x-www-browser x-www-browser /usr/bin/firefox 200
+update-alternatives --config x-www-browser
